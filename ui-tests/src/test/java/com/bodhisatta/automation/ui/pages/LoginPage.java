ab019a93272332de3dc65de6c2ea2e6216a7625e
Alpha <alpha@gmail.com>
2026-03-12 14:45:03 +0530

    Added more explicit wait to UI test

--- ui-tests/src/test/java/com/bodhisatta/automation/ui/pages/LoginPage.java
+++ ui-tests/src/test/java/com/bodhisatta/automation/ui/pages/LoginPage.java
@@ -45,7 +45,7 @@ public class LoginPage {
 
     public boolean isUserLoggedIn()
     {
-        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(40));
 
         WebElement loggedInElement=wait.until(ExpectedConditions.visibilityOfElementLocated(loggedInText));
 
